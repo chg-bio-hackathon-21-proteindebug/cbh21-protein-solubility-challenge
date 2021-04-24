d123347ccbac05ffd4ceafde9e9c4f68a308451e
Sancar Adali <sadali+git@gmail.com>
2021-04-24 19:53:58 -0400

    fix numpy array call

--- predict.py
+++ predict.py
@@ -288,7 +288,7 @@ def ml_inference(features: list[Any]) -> float:
     """
 
     loaded_model = load(MODEL_PATH)
-    feature_vec=np.array(0.0,len(features))
+    feature_vec = np.array([0.0]*len(features), dtype="float" )
     for feat_i, feat_val in enumerate(features):
         feature_vec[feat_i] = float(feat_val)
     pred_single = loaded_model.predict(feature_vec)
